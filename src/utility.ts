b35ca2433f8b597fabbb991217e09e103258f8ef
Axel Wettervik <axel@kidsloop.live>
2022-06-16 13:19:06 +0900

    chore: add typedoc for Utility functions

--- src/utility.ts
+++ src/utility.ts
@@ -1,6 +1,13 @@
 
+/**
+ * Check if the `UnrealWebViewBridge` is installed.
+ * @returns True if installed
+ */
 export const IsUnrealWebViewBridgeInstalled = () => window.ue?.unrealjsbridge !== undefined;
 
+/**
+ * Throw error if the `UnrealWebViewBridge` isn't installed.
+ */
 export function AssertUnrealWebViewBridgeInstalled() {
     if (!window.ue?.unrealjsbridge) {
         throw new Error(`This code isn't running within an Unreal Engine WebView compatible with \`ue-webview-bridge\``);
